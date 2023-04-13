ae5e657685b9538b5e6ff66d2d9fe18ce1564496
Red <red.sarouf2014@gmail.com>
2023-04-13 08:45:43 -0400

    Added Sending Data with Fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const div = document.createElement("div");
+    
+      document.querySelector("body").appendChild(div);
+      document.querySelector("div").textContent = data.id;
+    })
+    .catch(function (error) {
+      document.querySelector("body").textContent = error.message;
+    });
+}
